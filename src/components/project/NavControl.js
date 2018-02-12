4aa96a7a5e7075b88a2eb236416d0c1d8e34061c
Kevin Ports <kevin.ports@gmail.com>
2018-02-11 23:03:30 -0600

    Cleanup

--- src/components/project/NavControl.js
+++ src/components/project/NavControl.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 const transitionName = name || "";
+
 const NavControl = (props) => {
   const { last, next, name } = props;
   const transitionName = name || "";
