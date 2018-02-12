a31a18e65288d4bf8d2b958fbb9d2dd508a1ecaf
Kevin Ports <kevin.ports@gmail.com>
2018-02-12 11:26:30 -0600

    Update server for nicer logging and error handling

--- server.js
+++ server.js
@@ -1,11 +1,60 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const http = require('http');
 
-var app = express();
+const port = normalizePort(process.env.PORT || '3001');
+const app = express();
 
 app.use(express.static('./www'));
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname + '/www/index.html'));
 });
 
-app.listen(3001);
+const server = http.createServer(app);
+server.listen(3001);
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
